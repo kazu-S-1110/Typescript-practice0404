dc52bcdd96817cb32189738a81c8a07039e042ad
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-07 12:28:58 +0900

    unionとintersectionを練習

--- dist/object/alias.js
+++ dist/object/alias.js
@@ -7,5 +7,31 @@ function typeAliasSample() {
         name: "Japan"
     };
     console.log(japan);
+    var america = {
+        capital: "Washington,D.C.",
+        language: "Eng",
+        name: "United State of America"
+    };
+    console.log(america);
+    var adventurer1 = {
+        hp: 100,
+        mp: 40,
+        weapon: "stuff",
+        magicSkill: "fire"
+    };
+    var adventurer2 = {
+        hp: 100,
+        sp: 40,
+        weapon: "spire",
+        swordSkill: "slash"
+    };
+    var paladin = {
+        hp: 300,
+        mp: 100,
+        sp: 30,
+        weapon: "sword",
+        swordSkill: "slash",
+        magicSkill: "thunder"
+    };
 }
 exports.default = typeAliasSample;
--- src/object/alias.ts
+++ src/object/alias.ts
@@ -11,4 +11,50 @@ export default function typeAliasSample() {
     name:"Japan"
   }
   console.log(japan)
+
+  const america: Country = {
+    capital: "Washington,D.C.",
+    language: "Eng",
+    name: "United State of America"
+  }
+  console.log(america)
+
+  // 合併型(union)と交差型(intersection)
+  type Knight = {
+    hp: number
+    sp: number
+    weapon: string
+    swordSkill:string
+  }
+  type Wizard = {
+    hp: number
+    mp: number
+    weapon: string
+    magicSkill:string
+  }
+  // 合併型...KnightまたはWizardの型を持てる
+  type Adventurer = Knight | Wizard
+  //交差型...KnightかつWizardの型を持てる
+  type Paladin = Knight & Wizard
+
+  const adventurer1: Adventurer = {
+    hp: 100,
+    mp:40,
+    weapon: "stuff",
+    magicSkill: "fire"
+  }
+  const adventurer2: Adventurer = {
+    hp: 100,
+    sp:40,
+    weapon: "spire",
+    swordSkill: "slash"
+  }
+  const paladin: Paladin = {
+    hp: 300,
+    mp: 100,
+    sp: 30,
+    weapon: "sword",
+    swordSkill: "slash",
+    magicSkill:"thunder"
+  }
 }
