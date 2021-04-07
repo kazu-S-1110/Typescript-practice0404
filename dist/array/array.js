fa8e6dfcf56fb4a4f18aa33ee0b9a1b644f701b5
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-08 05:59:35 +0900

    別の配列の型定義方法を練習

--- dist/array/array.js
+++ dist/array/array.js
@@ -6,5 +6,10 @@ function arraySample() {
     colors.push("yellow");
     // colors.push(123) 数値型だからできないよ
     console.log(colors);
+    // 別の配列の型定義方法
+    var even = [2, 4, 6, 8, 10];
+    even.push(10);
+    // even.push("dd") 文字列型だからできないよ
+    console.log(even);
 }
 exports.default = arraySample;
--- src/array/array.ts
+++ src/array/array.ts
@@ -5,7 +5,11 @@ export default function arraySample() {
   // colors.push(123) 数値型だからできないよ
   console.log(colors)
 
-
+  // 別の配列の型定義方法
+  const even: Array<number> = [2, 4, 6, 8, 10]
+  even.push(10)
+  // even.push("dd") 文字列型だからできないよ
+  console.log(even)
 
 
 
