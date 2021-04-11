61d5bdc15b57691333aa730405f4ba7a6b8a6bb9
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-11 21:15:29 +0900

    eslint-fix実行時にprettierも実行させるよう記述

--- src/function/basic.ts
+++ src/function/basic.ts
@@ -1,24 +1,24 @@
 // アロー関数
 export const logMessage = (message: string): void => {
-  console.log(message);
-};
+  console.log(message)
+}
 
 // 名前付き関数
 export function logMessage2(message: string): void {
-  console.log(message);
+  console.log(message)
 }
 
 // 関数式
 export const logMessage3 = function (message: string): void {
-  console.log(message);
-};
+  console.log(message)
+}
 
 // アロー関数の省略記法
-export const logMessage4 = (message: string): void => console.log(message);
+export const logMessage4 = (message: string): void => console.log(message)
 
 export const alwaysThrowError = (message: string): never => {
-  throw new Error(message);
-};
+  throw new Error(message)
+}
 
 // 呼び出しシグネチャ（省略記法）
 type LogMessage = (message: string) => void
@@ -26,6 +26,6 @@ export const logMessage5: LogMessage = (message) => console.log(message)
 
 // 完全な呼び出しシグネチャ
 type FullLogMessage = {
-  (message:string):void
+  (message: string): void
 }
-export const logMessage6:FullLogMessage =(message) => console.log(message)
+export const logMessage6: FullLogMessage = (message) => console.log(message)
--- src/function/parameters.ts
+++ src/function/parameters.ts
@@ -1,32 +1,27 @@
 // オプションパラメータを持つ関数
 export const isUserSignedIn = (userId: string, username?: string): boolean => {
   if (userId === 'ABC') {
-    console.log('User is signed in! Username is', username);
-    return true;
+    console.log('User is signed in! Username is', username)
+    return true
   } else {
-    console.log('failed');
-    return false;
+    console.log('failed')
+    return false
   }
-};
+}
 // デフォルトパラメータを持つ関数
-export const isUserSignedIn2 = (
-  userId: string,
-  username = 'No NAME'
-): boolean => {
+export const isUserSignedIn2 = (userId: string, username = 'No NAME'): boolean => {
   if (userId === 'ABC') {
-    console.log('User is signed in! Username is', username);
-    return true;
+    console.log('User is signed in! Username is', username)
+    return true
   } else {
-    console.log('failed');
-    return false;
+    console.log('failed')
+    return false
   }
-};
+}
 
 // レストパラメータを持つ関数
 export const sumProductsPrice = (...productPrice: number[]): number => {
   return productPrice.reduce((prevTotal: number, productPrice: number) => {
-    return prevTotal + productPrice;
-  });
-};
-
-
+    return prevTotal + productPrice
+  })
+}
